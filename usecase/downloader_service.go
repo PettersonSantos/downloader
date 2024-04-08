65660a7c606d8fab982e9257a574a37dc9ad9914
Petterson Luis da Silva Santos <petterson.santos@outlook.com.br>
2024-04-08 20:21:00 -0300

    add download's progress

--- usecase/downloader_service.go
+++ usecase/downloader_service.go
@@ -2,11 +2,13 @@ package usecase
 
 import (
 	"errors"
+	"fmt"
 	"io"
 	"net/http"
 	"net/url"
 	"os"
 	"path"
+	"strconv"
 	"sync"
 )
 
@@ -19,7 +21,6 @@ type EventServiceImpl struct {
 
 func (impl *EventServiceImpl) ProcessDownload(urls []string, dirPath string) {
 
-	done := make(chan bool, len(urls))
 	errch := make(chan error, len(urls))
 
 	var wg sync.WaitGroup
@@ -28,47 +29,84 @@ func (impl *EventServiceImpl) ProcessDownload(urls []string, dirPath string) {
 	for _, URL := range urls {
 		go func(URL string) {
 			defer wg.Done()
-			b, err := download(URL, dirPath)
+			err := download(URL, dirPath)
 			if err != nil {
 				errch <- err
 				return
 			}
-			done <- b
 		}(URL)
 	}
+
+	var qtdErr int
+	for err := range errch {
+		if err != nil {
+			qtdErr++
+			fmt.Println("Erro ao baixar arquivo:", err)
+		}
+	}
+	fmt.Printf("%d/%d downloads concluídos com sucesso\n", len(urls)-qtdErr, len(urls))
+
 	wg.Wait()
-	close(done)
 	close(errch)
 }
 
-func download(url string, dirPath string) (bool, error) {
+func download(url string, dirPath string) error {
 	fileName, err := extractFileNameFromURL(url)
 	if err != nil {
-		return false, err
+		return err
 	}
 
 	filePath := path.Join(dirPath, fileName)
 
 	resp, err := http.Get(url)
 	if err != nil {
-		return false, err
+		return err
 	}
+	totalSize, _ := strconv.Atoi(resp.Header.Get("Content-Length"))
 	defer resp.Body.Close()
 	if resp.StatusCode != http.StatusOK {
-		return false, errors.New(resp.Status)
+		return errors.New(resp.Status)
 	}
 
 	out, err := os.Create(filePath)
 	if err != nil {
-		return false, err
+		return err
 	}
 	defer out.Close()
 
 	_, err = io.Copy(out, resp.Body)
 	if err != nil {
-		return false, err
+		return err
 	}
-	return true, nil
+
+	err = showProgress(resp, totalSize, out, url)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func showProgress(resp *http.Response, totalSize int, out *os.File, url string) error {
+	var downloaded int
+	buffer := make([]byte, 1024)
+	for {
+		n, err := resp.Body.Read(buffer)
+		if n > 0 {
+			downloaded += n
+			out.Write(buffer[:n])
+			percentComplete := float64(downloaded) / float64(totalSize) * 100
+			fmt.Printf("\rDownloading %s: %.2f%% complete", url, percentComplete)
+		}
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
 }
 
 func extractFileNameFromURL(fileURL string) (string, error) {
