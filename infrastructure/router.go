4159bf57fcd3cc4936593688b01150ce0b2cc4f9
Petterson Luis da Silva Santos <petterson.santos@outlook.com.br>
2024-04-08 22:25:43 -0300

    create controller structure

--- infrastructure/router.go
+++ infrastructure/router.go
@@ -1,5 +1,12 @@
 package infrastructure
 
-func Init() {
+import (
+	"github.com/go-chi/chi/v5"
+	controllers "main/interfaces/api"
+)
 
+func AppendDownloaderRoute(controller controllers.DownloaderController) func(router chi.Router) {
+	return func(router chi.Router) {
+		router.Post("/", controller.CreateDownloader)
+	}
 }
--- interfaces/api/downloader_controller.go
+++ interfaces/api/downloader_controller.go
@@ -1,1 +1,35 @@
 package controllers
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"main/usecase"
+	"net/http"
+)
+
+type DownloaderController interface {
+	CreateDownloader(w http.ResponseWriter, r *http.Request)
+}
+
+type ControllerDownloader struct {
+	service usecase.IDownloaderService
+}
+
+func (impl *ControllerDownloader) CreateDownloader(w http.ResponseWriter, r *http.Request) {
+	var urls []string
+	bodyBytes, err := io.ReadAll(r.Body)
+	if err != nil {
+		return
+	}
+	if err := json.Unmarshal(bodyBytes, urls); err != nil {
+		_ = fmt.Errorf("error to unmarshal body %d", err)
+	}
+	impl.service.ProcessDownload(urls, "/test")
+}
+
+func NewDownloaderController(service usecase.IDownloaderService) DownloaderController {
+	return &ControllerDownloader{
+		service: service,
+	}
+}
